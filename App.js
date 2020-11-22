0519aff062f962cdc0cbca790d8b146af75cd879
Gustavo <gustavoacacio22@gmail.com>
2020-11-22 17:00:13 -0300

    Camera dicionada

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Contatos from './Pages/Contatos';
 import TextToSpeech from './Pages/TextToSpeech';
+import ImagemCamera from './Pages/Camera';
 
 
 const Tab = createBottomTabNavigator();
@@ -14,6 +15,7 @@ export default function App() {
       <Tab.Navigator>
         <Tab.Screen name="Contatos" component={Contatos}/>
         <Tab.Screen name="TextToSpeech" component={TextToSpeech}/>
+        <Tab.Screen name="ImagemCamera" component={ImagemCamera}/>
       </Tab.Navigator>
     </NavigationContainer>
   );
